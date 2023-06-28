ceb2bd689e595ed1f30878c043a8b8ad0df5ab40
Elton Campos <eltoncampos36@gmail.com>
2023-06-28 12:39:12 -0300

    test: setSocket should store io instance

--- g-drive-webapi/test/unit/routes.test.js
+++ g-drive-webapi/test/unit/routes.test.js
@@ -1,7 +1,17 @@
 import {describe, test, expect} from "@jest/globals"
+import Routes from "../../src/routes.js"
 
-describe("", () => {
-    test("", () => {
+describe("Routes test suite", () => {
+    describe("setSocketInstance", () => {
+        test("setSocket should store io instance", () => {
+            const routes = new Routes()
+            const ioObj = {
+                to: (id) => ioObj,
+                emit: (event, message) => {}
+            }
 
+            routes.setSocketInstance(ioObj)
+            expect(routes.io).toStrictEqual(ioObj)
+        })
     })
 })
